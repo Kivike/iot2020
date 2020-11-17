2a2762cffddc7fc26ba2661f4ed5836c57d85688
Worpaaja <lasse_780@hotmail.com>
2020-11-17 20:32:24 +0200

    connection works now, next up what is characterisUUId

--- iotclient.py
+++ iotclient.py
@@ -3,22 +3,35 @@ from bleak import BleakScanner
 from bleak import BleakClient
 
 
+CHARACTERISTIC_UUID = "f000aa65-0451-4000-b000-000000000000"  # <--- Change to the characteristic you want to enable notifications from.
+
+def notification_handler(sender, data):
+    """Simple notification handler which prints the data received."""
+    print("{0}: {1}".format(sender, data))
+
 #Scan for devices
 async def run():
-    devices = await BleakScanner.discover()
-    i = 1
-    for d in devices:
-        print("(", i, ")", d, "\n")
-        i = i + 1
-    selection = input("Which device do you want to use?")
-    client = BleakClient(devices[selection-1])
-    try:
-        await client.connect()
-    except Exception as e:
-        print(e)
-    finally:
-        print("Connection status: ", client.is_connected())
-        print("Reading in action: ", client.read_gatt_char())
+    address = "66:80:4D:1A:2F:03" #address if you want to use specific device, oneplus = 66:80:4D:1A:2F:03
+    use_spef_dev = 0              
+    if(use_spef_dev == 0):        #Discovery step, can be skipped with use_spef_dev == 1
+        devices = await BleakScanner.discover()
+        i = 1
+        for d in devices:
+            print("({0}):  {1}\n".format(i, d))
+            i = i + 1
+        selection = int(input("Which device do you want to use? "))
+        address = devices[selection - 1]
+
+    async with BleakClient(address) as client:
+        connected = await client.is_connected()
+        print("Connection status: ", connected)
+
+        await client.start_notify(CHARACTERISTIC_UUID, notification_handler)
+        print("Notify started")
+        #await write_gatt_descriptor(,)	#handle, data. Dataan start sensing
+        await asyncio.sleep(5.0)
+        await client.stop_notify(CHARACTERISTIC_UUID)
+        print("Notify stopped")
         await client.disconnect()
 
 def main():
