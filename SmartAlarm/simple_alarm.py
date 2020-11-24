2882db9df6f3ef9e903fe377b064203571412701
Worpaaja <lasse_780@hotmail.com>
2020-11-24 19:06:41 +0200

    updated alarm file

--- SmartAlarm/simple_alarm.py
+++ SmartAlarm/simple_alarm.py
@@ -8,12 +8,24 @@ import sys
 from bleak import BleakScanner
 from bleak import BleakClient
 
+class Timer():
+    def __init__(self):
+        self.current_time = time.time()
+        
+    def reset_time(self):
+        self.current_time = time.time()
+    
+    def get_time(self):
+        time = time.time() - current_time
+        return time
+        
 
 def main():
     wake_up_time = input("When do you want to wake up? HH:MM:SS ")
     wake_up_time = wake_up_time.split(":")
 
     while(True):
+        time.sleep(3)
         current_time = datetime.datetime.now()
         now = current_time.strftime("%H:%M:%S")
         now = now.split(":")
@@ -22,37 +34,31 @@ def main():
             client = BleClient(test_callback) 
             loop = asyncio.get_event_loop()
             winsound.PlaySound("soundalarm.wav",  winsound.SND_ASYNC)  #Alarm sound, this should play until if(test_callback()) return True
-
             try:
                 loop.run_until_complete(client.run(loop))
             except KeyboardInterrupt:
                 # Handle Ctrl+C
                 loop.run_until_complete(client.stop())
-            timer = 0;
-            while(True):            #if ble server sends light are on, stop sound
-                time.sleep(1)
-                
-                if(test_callback()):               #This should stop the alarm
-                    winsound.PlaySound("soundalarm.wav",  winsound.SND_PURGE)
-                    timer +=1
-                elif():               #This should restart the alarm and reset the timer
-                    winsound.PlaySound("soundalarm.wav",  winsound.SND_ASYNC)
-                    timer = 0
-                else:
-                    print("You shouldn't be here.")
-                if(timer == 120):    
-                    break
-            break
     print("Good morning")
 
 def test_callback(value): 
     if (value):
         print("Lighs turned on")
-        return True
+        winsound.PlaySound("soundalarm.wav",  winsound.SND_PURGE)
+        timer = Timer()
+        while(True):
+            if(!value):
+                timer = 0
+                winsound.PlaySound("soundalarm.wav",  winsound.SND_ASYNC)
+            if(timer.get_time() >= 60):
+                break
+            timer += 1
+            time.sleep(1)
         # Wait 60 sec before turning off sensor
+        
     else:
         print("Lights turned off")
-        return False
+        winsound.PlaySound("soundalarm.wav",  winsound.SND_ASYNC)
         # Start alarm again
         
 if __name__ == "__main__":
